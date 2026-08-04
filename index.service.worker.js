9615aa28983607500a62ae66ab25ecacab3a6c20
scudmarx <darth_dunk@hotmail.com>
2026-08-04 03:44:21 +0100

    28.9

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785806935|17301303';
+const CACHE_VERSION = '1785811421|14293549';
 /** @type {string} */
 const CACHE_PREFIX = 'Open Table MVP v-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
