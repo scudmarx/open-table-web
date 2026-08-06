80d510efd979708077d1cfc747fb9fcb9adedfd2
scudmarx <darth_dunk@hotmail.com>
2026-08-06 06:20:26 +0100

    28.12.4

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785992908|17101973';
+const CACHE_VERSION = '1785993606|13811798';
 /** @type {string} */
 const CACHE_PREFIX = 'OpenTable-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
